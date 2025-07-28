3321c1cf76007012c6fb0b5c1fff87f8546ebb46
ElectronicHimate <107193409+ElectronicHimate@users.noreply.github.com>
2025-07-28 13:14:05 +1000

    Revert "Code refactor"

--- app/src/main/java/org/freedesktop/wayland/WlPointerButtonState.java
+++
@@ -1,6 +0,0 @@
-package org.freedesktop.wayland;
-
-public class WlPointerButtonState {
-    public static final int WL_POINTER_BUTTON_STATE_RELEASED = 0;
-    public static final int WL_POINTER_BUTTON_STATE_PRESSED = 1;
-}
